d91384ef63a080186dbaa3f8c4dc448415e58d7f
TOKIO5025 <albertodovi100@gmail.com>
2025-07-19 18:59:39 -0600

    Create grupo-link.js

---
+++ plugins/grupo-link.js
@@ -0,0 +1,27 @@
+import fs from 'fs';
+const handler = async (m, { conn, args }) => {
+  const group = m.chat;
+
+  // Enviar el enlace del grupo con estilo
+  await m.reply(`✨ *Aquí tienes el acceso al nido del caos~* 😈🔥\n\nhttps://chat.whatsapp.com/${await conn.groupInviteCode(group)}`);
+
+  // Espera 1 segundo antes de enviar el audio
+  await delay(1000);
+
+  // Enviar audio tipo PTT (voz)
+  await conn.sendMessage(m.chat, {
+    audio: { url: 'https://o.uguu.se/KgNJACqk.mp3' },
+    mimetype: 'audio/mpeg',
+    ptt: true
+  }, { quoted: m });
+};
+
+handler.help = ['linkgroup'];
+handler.tags = ['group'];
+handler.command = /^link(gro?up)?$/i;
+handler.group = true;
+handler.botAdmin = true;
+handler.register = true;
+export default handler;
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
