92f4af29ad68b6fe4b8fb921a9de6b953eed1372
TOKIO5025 <albertodovi100@gmail.com>
2025-07-21 16:16:42 -0600

    Update Nsfw-tetas.js

--- plugins/Nsfw-tetas.js
+++ plugins/Nsfw-tetas.js
@@ -1,18 +1,27 @@
 import fetch from 'node-fetch';
 
 const handler = async (m, { conn }) => {
-  const res = await fetch('https://nekos.life/api/v2/img/boobs');
-  const json = await res.json();
+  try {
+    const res = await fetch('https://api.waifu.pics/nsfw/boobs');
+    const json = await res.json();
 
-  await conn.sendMessage(m.chat, {
-    image: { url: json.url },
-    caption: `🍒 Tómate estas tetas, cochino 😏`,
-  }, { quoted: m });
+    if (!json.url) throw 'No se encontró imagen';
+
+    await conn.sendMessage(m.chat, {
+      image: { url: json.url },
+      caption: `🍒 Aquí tienes tus tetas, puerquito 😏\n_Disfrútalo, degenerado._`,
+    }, { quoted: m });
+  } catch (e) {
+    console.error(e);
+    await m.reply('❌ No se pudo obtener la imagen. La API está caída o respondió mal.');
+  }
 };
 
 handler.command = ['tetas'];
 handler.tags = ['nsfw'];
 handler.help = ['tetas'];
 handler.register = true;
+handler.private = false;
+handler.group = false;
 
 export default handler;
