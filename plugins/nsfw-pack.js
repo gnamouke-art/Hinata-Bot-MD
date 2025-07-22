6830802f0c76ea3c965fc799669287ee18868023
TOKIO5025 <albertodovi100@gmail.com>
2025-07-22 15:17:24 -0600

    Update nsfw-pack.js

--- plugins/nsfw-pack.js
+++ plugins/nsfw-pack.js
@@ -1,27 +1,25 @@
 //código creado por tu jefe 🐉𝙉𝙚𝙤𝙏𝙤𝙆𝙮𝙤 𝘽𝙚𝙖𝙩𝙨🐲
-//para Hinata Bot deja créditos pa
 import fetch from 'node-fetch';
 
 const handler = async (m, { conn, command, usedPrefix }) => {
   try {
-    const res = await fetch('https://api.waifu.pics/nsfw/waifu');
+    const res = await fetch('https://nekos.life/api/v2/img/lewd');
     const json = await res.json();
 
     await conn.sendMessage(m.chat, {
       image: { url: json.url },
-      caption: `🥵 *${usedPrefix || ''}${command || 'pack'}*\n📦 Aquí tienes tu pack asquerosito 😈\n\nDisfruta, pervertid@...`,
+      caption: `🥵 *${usedPrefix || ''}${command || 'pack'}*\n📦 Aquí tienes otro pack bien sucio 😈\n\nTe gusta lo pervertido, ¿eh?`,
     }, { quoted: m });
   } catch (e) {
-    await conn.reply(m.chat, '❎ No pude traer el pack, intenta más tarde 💔', m);
+    await conn.reply(m.chat, '❎ No pude traerte el pack ahora mismo 💔', m);
     console.error(e);
   }
 };
 
-// Soporta: pack, .pack, !pack, 🥵pack, 🥵.pack, etc.
 handler.customPrefix = /^([🥵]?\s*([./!])?\s*pack)$/i;
-handler.command = new RegExp; // para que use customPrefix
+handler.command = new RegExp;
 handler.tags = ['nsfw'];
-handler.help = ['pack'];
+handler.help = ['🥵pack'];
 handler.register = true;
 
 export default handler;
