2018999dbab98802da6e1d41a2e6f4d727a0b4cc
TOKIO5025 <albertodovi100@gmail.com>
2025-07-23 13:44:09 -0600

    Update Botones.js

--- plugins/Botones.js
+++ plugins/Botones.js
@@ -1,15 +1,17 @@
 let handler = async (m, { conn }) => {
-  await conn.sendMessage(m.chat, {
-    text: `✨ *¡Hola guapo!* Soy 𝐇𝐈𝐍𝐀𝐓𝐀-𝐁𝐎𝐓. ¿Qué deseas hacer hoy? 😏`,
-    footer: 'Desarrollado por 🐉𝙉𝙚𝙤𝙏𝙤𝙆𝙮𝙤 𝘽𝙚𝙖𝙩𝙨🐲 & light Yagami',
+  const message = {
+    text: `✨ *¡Hola guapo!* Soy *𝐇𝐈𝐍𝐀𝐓𝐀-𝐁𝐎𝐓*. ¿Qué deseas hacer hoy? 😏
+
+Desarrollado por 🐉𝙉𝙚𝙤𝙏𝙤𝙆𝙮𝙤 𝘽𝙚𝙖𝙩𝙨🐲 & light Yagami`,
+    footer: '👑 Owner',
     buttons: [
       { buttonId: '.menu', buttonText: { displayText: '📜 Ver Menú' }, type: 1 },
-      { buttonId: '.estado', buttonText: { displayText: '📊 Estado Bot' }, type: 1 },
-      { buttonId: '.owner', buttonText: { displayText: '👑 Owner' }, type: 1 }
+      { buttonId: '.estado', buttonText: { displayText: '📊 Estado Bot' }, type: 1 }
     ],
     headerType: 1
-  }, { quoted: m })
+  }
+  await conn.sendMessage(m.chat, message, { quoted: m })
 }
 
-handler.command = /^hinatamenu|menuhinata|botmenu$/i
+handler.command = /^hinatamenu$/i
 export default handler
