3a5ea4782648704ff7a0b87b7abe07bcfff6f88c
TOKIO5025 <albertodovi100@gmail.com>
2025-07-23 15:13:02 -0600

    Update admin-restri.js

--- plugins/admin-restri.js
+++ plugins/admin-restri.js
@@ -1,10 +1,26 @@
-let handler = async (m, { conn, text, isOwner }) => {
-  if (!isOwner) throw 'Este comando es solo para mi dueño 🙄'
-  global.db.data.settings[conn.user.jid].restrict = true
-  m.reply('✅ Restricción activada. Ahora puedo expulsar 🦵💢')
+let handler = async (m, { conn, args, isOwner }) => {
+  if (!isOwner) throw 'Este comando es solo para mis dioses, no insistas 🙄'
+
+  const setting = global.db.data.settings[conn.user.jid] || {}
+
+  if (!args[0]) {
+    return m.reply(`📦 *Restricción actual:* ${setting.restrict ? 'activada ✅' : 'desactivada ❌'}\n\nUsa:\n- .restrict enable\n- .restrict disable`)
+  }
+
+  if (args[0] === 'enable') {
+    setting.restrict = true
+    m.reply('✅ *Restricción activada.* Ahora puedo expulsar a los pecadores 🔥')
+  } else if (args[0] === 'disable') {
+    setting.restrict = false
+    m.reply('❌ *Restricción desactivada.* Ya no expulsaré a nadie, todos libres como el viento 🍃')
+  } else {
+    m.reply('⚠️ Uso incorrecto. Escribe:\n.restrict enable\n.restrict disable')
+  }
 }
-handler.help = ['restrict on']
+
+handler.help = ['restrict <enable|disable>']
 handler.tags = ['owner']
-handler.command = /^restrict\s?(on)?$/i
-handler.owner = true
+handler.command = /^restrict$/i
+handler.rowner = true
+
 export default handler
